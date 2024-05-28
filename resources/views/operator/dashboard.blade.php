876fe691e09cda4b0ab0369fd6361c45cc3340db
yudhit08 <yudhitrenandra99@gmail.com>
2024-05-28 14:10:53 +0700

    edit color

--- resources/views/operator/dashboard.blade.php
+++ resources/views/operator/dashboard.blade.php
@@ -34,20 +34,6 @@
     <div class="w-full p-5 h-full">
         <h2 class="text-gray-600 font-semibold text-2xl">Isikan Capaian Kinerja Triwulan Ke-{{ $triwulan }}</h2>
         <p class="text-gray-600 font-light text-lg mt-2">{{ $userBidang }}</p>
-        <div class="mt-10 flex items-center gap-2">
-            <p class="text-gray-800">Pilih triwulan:</p>
-            <div class="flex items-center">
-                <select class="px-4 py-2 pr-4 w-[200px] rounded-md shadow-sm outline-none border-none appearance-none text-gray-800 active:border-blue-500 active:border-2">
-                    <option>Triwulan I</option>
-                    <option>Triwulan II</option>
-                    <option>Triwulan III</option>
-                    <option>Triwulan IV</option>
-                </select>
-                <svg class="w-4 h-4 mt-px -ml-6 pointer-events-none " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                </svg>
-            </div>
-        </div>
         <div class="mt-10 bg-white p-5 rounded shadow">
             <ul class="flex flex-col gap-4">
                 <li class="">
